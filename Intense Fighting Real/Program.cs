cd222ac6f2b0c7057d4c266327b4635a33a18de0
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2023-10-04 10:43:59 +0200

    Fixed NumberTest all on my own without any help

--- Intense Fighting Real/Program.cs
+++ Intense Fighting Real/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace StandardRPG{
     using System.Formats.Asn1;
     using System.Reflection.Metadata.Ecma335;
+    using System.Runtime.InteropServices;
     using static System.Console;
     class StandardRPGRunState{
         public static Enemy player = new Enemy();
@@ -10,18 +11,18 @@
         static void Start(){
             WriteLine("What's your username?");
             player.name = ReadLine();
-            int IntTest;
-            bool NumberTest = int.TryParse(player.name, out IntTest);
+            bool NumberTest = player.name.Any(Char.IsDigit);
+            WriteLine(NumberTest);
             if (NumberTest == true || player.name.Length < 4 || player.name.Length > 16){
                 while (NumberTest == true || player.name.Length < 4 || player.name.Length > 16){
                     if (NumberTest == true && player.name.Length < 4){
-                        WriteLine("Name cannot exclusively contain numbers or be less than 4 characters.");
+                        WriteLine("Name cannot contain numbers or be less than 4 characters.");
                     }
-                    else if (NumberTest == true && player.name.Length > 16){
-                        WriteLine("Name cannot exclusively contain numbers or exceed 16 characters.");
+                    if (NumberTest == true && player.name.Length > 16){
+                        WriteLine("Name cannot contain numbers or exceed 16 characters.");
                     }   
                     else if (NumberTest == true){
-                        WriteLine("Name cannot exclusively contain numbers.");
+                        WriteLine("Name cannot contain numbers.");
                     }
                     else if (player.name.Length < 4){
                         WriteLine("Name cannot be less than 4 characters.");
@@ -30,7 +31,7 @@
                         WriteLine("Name cannot exceed 16 characters.");
                     }
                     player.name = ReadLine();
-                    NumberTest = int.TryParse(player.name, out IntTest);
+                    NumberTest = player.name.Any(Char.IsDigit);
                 }
             }
             if (player.name is string){
