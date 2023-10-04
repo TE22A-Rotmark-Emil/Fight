e4336c1a1eda856ae0293c72366ac58baafa0eb6
Emil Rotmark <emil.rotmark@elev.ga.ntig.se>
2023-10-04 11:03:29 +0200

    Did half the work required to create enemies

--- Intense Fighting Real/Enemy.cs
+++ Intense Fighting Real/Enemy.cs
@@ -1,9 +1,28 @@
-namespace StandardRPG{
-    class Enemy{
+namespace StandardRPG
+{
+    class Enemy
+    {
+        public static Enemy player;
+
         public string? name;
+        public float enemyPower;
         public int Health = 20;
         public int Attack = 1;
-        public int Defence = 0;
+        public int Defence = 1;
         public float DodgeChance = 0.25f;
+        Random generator = new Random();
+
+        public void EnemyController()
+        {
+            if (this != player)
+            {
+                enemyPower = generator.Next(2,5) * 0.1f;
+                Health = (int)(Health * enemyPower);
+                Attack = (int)(Attack * enemyPower);
+                Defence = 0;
+                DodgeChance *= enemyPower;
+                
+            }
+        }
     }
 }
